f6bbffaa60a7e1b64e0803c643dcbbcdb07bdb4d
Vy Yen <nhingayen@gmail.com>
2020-01-09 10:28:21 +0700

    implement sendmail function

--- sendemail.php
+++ sendemail.php
@@ -1,4 +1,40 @@
 <?php
 
-var_dump($_POST);
+require 'vendor/autoload.php';
 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.migadu.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'vyyen@rustspawn.com';                     // SMTP username
+    $mail->Password   = 'vyyen123';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('vyyen@rustspawn.com', 'VyYen');
+    $mail->addAddress('nhingayen@gmail.com', 'nhi');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['message'];
+    $mail->AltBody = $_POST['message'];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
